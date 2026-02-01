60b7fe71b3bb32007057b563816ee9f3d0b9b756
Polina Eletskaia <polina.e@compsscollege.art>
2026-02-01 15:00:46 +0600

    list

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,28 @@
 import { MovieItem } from "../MovieItem/MovieItem"
+import { LikeDislike } from "../LikeDislike/LikeDislike" 
 
-export const MovieList = (props) => {
+export const MovieList = ({movies, onToggleWatched, onLike, onDislike}) => {
   return (
-    <div>
-      {props.movies.map((movie) => (
-        <MovieItem movie={movie} key={movie.id} onDelete={props.onDeleteMovie}/>
+     <div>
+      {movies.map(movie => (
+        <div key={movie.id}>
+          <MovieItem 
+            title={movie.title}
+            watched={movie.watched}
+          />
+
+          <button onClick={() => onToggleWatched(movie.id)}>
+            {movie.watched ? "✅ watched" : "mark watched"}
+          </button>
+
+          
+            <LikeDislike
+              likeStatus={movie.likeStatus}
+              onLike={() => onLike(movie.id)}
+              onDislike={() => onDislike(movie.id)}
+            />
+          
+        </div>
       ))}
     </div>
   )
